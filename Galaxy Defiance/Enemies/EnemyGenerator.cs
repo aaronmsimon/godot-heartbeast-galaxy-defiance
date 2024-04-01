e86215e0bc08f2c6cc3e014deaf8e04915dac7db
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2024-04-01 11:53:45 -0700

    P17 Difficulty Curve
    Video 17 in series https://www.youtube.com/playlist?list=PL9FzW-m48fn09w6j8NowI_pSBVcsb3V78
    
    Desmos.com is a handy site to graph functions

--- Galaxy Defiance/Enemies/EnemyGenerator.cs
+++ Galaxy Defiance/Enemies/EnemyGenerator.cs
@@ -1,12 +1,21 @@
+using System;
 using Godot;
 
 public partial class EnemyGenerator : Node2D
 {
+    [ExportGroup("Margin Settings")]
     [Export] private int margin = 8;
     [Export] private int topMargin = 16;
+
+    [ExportGroup("Difficulty Settings")]
+    [Export] private int yellowMinScore = 10;
+    [Export] private int pinkMinScore = 50;
+
+    [ExportGroup("Game Data")]
     [Export] private PackedScene greenEnemyScene;
     [Export] private PackedScene yellowEnemyScene;
     [Export] private PackedScene pinkEnemyScene;
+    [Export] private GameStats gameStats;
 
     private SpawnerComponent spawnerComponent;
     private Timer greenEnemySpawnTimer;
@@ -23,14 +32,27 @@ public partial class EnemyGenerator : Node2D
         pinkEnemySpawnTimer = GetNode<Timer>("PinkEnemySpawnTimer");
 
         greenEnemySpawnTimer.Timeout += () => HandleSpawn(greenEnemyScene, greenEnemySpawnTimer);
-        yellowEnemySpawnTimer.Timeout += () => HandleSpawn(yellowEnemyScene, yellowEnemySpawnTimer);
-        pinkEnemySpawnTimer.Timeout += () => HandleSpawn(pinkEnemyScene, pinkEnemySpawnTimer);
+        yellowEnemySpawnTimer.Timeout += () => HandleSpawn(yellowEnemyScene, yellowEnemySpawnTimer, 5f);
+        pinkEnemySpawnTimer.Timeout += () => HandleSpawn(pinkEnemyScene, pinkEnemySpawnTimer, 10f);
+
+        gameStats.ScoreChanged += (int newScore) => {
+            if (newScore > yellowMinScore)
+            {
+                yellowEnemySpawnTimer.ProcessMode = ProcessModeEnum.Inherit;
+            }
+            if (newScore > pinkMinScore)
+            {
+                pinkEnemySpawnTimer.ProcessMode = ProcessModeEnum.Inherit;
+            }
+        };
     }
 
-    private void HandleSpawn(PackedScene enemyScene, Timer timer)
+    private void HandleSpawn(PackedScene enemyScene, Timer timer, float timeOffset = 1f)
     {
         spawnerComponent.Scene = enemyScene;
         spawnerComponent.Spawn(new Vector2(GD.RandRange(margin, screenWidth - margin), -topMargin));
-        timer.Start();
+
+        float spawnRate = timeOffset / (0.5f + (gameStats.Score * 0.01f)); // demonstrated with Desmos.com
+        timer.Start(spawnRate + GD.RandRange(0.25f, 0.5f));
     }
 }
